223fd7cb2cb3661969724684ad331ee1f71d137b
anzein-zayn <andy.zaky.ft24@mail.umy.ac.id>
2025-03-18 21:43:32 +0700

    Perulangan While

--- ContohInsertionSort.cpp
+++ ContohInsertionSort.cpp
@@ -5,3 +5,15 @@ int arr [20]; //Membuat Array dengan panjang data 20
 int n; // Membuat variable inputan n
 
 void input ()
+{
+    while (true)
+    {
+        cout<<" Masukkan Jumlah Data pada Array = "; //Membuat Inputan Jumlah element Array
+        cin >> n; //Memanggil variable inputan n
+
+        if (n <= 20)
+        {// Membuat Kondisi n tidak lebih dari 20
+            break;
+        }
+        else
+    {
