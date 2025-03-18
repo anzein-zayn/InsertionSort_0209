0e72176521a9804495effe3a81ca40d6acbc2ff5
anzein-zayn <andy.zaky.ft24@mail.umy.ac.id>
2025-03-18 21:59:27 +0700

    Procces For

--- ContohInsertionSort.cpp
+++ ContohInsertionSort.cpp
@@ -37,3 +37,19 @@ void insertionSort()
 
     int temp; // Membuat variable data temporer atau penyimpanan sementara
     int j, i; // Membuat variable j sebagai penanda
+
+    for (i = 1; i <= n - 1; i++)
+    {//Step 1
+        temp = arr[i];//Step 2
+
+        j = i - 1; //Step 3
+
+        while (j >= 0 && arr [j] > temp)//Step 4
+        {
+            arr[j + 1] = arr[j];//Step 4.a
+            j--;//Step 4.b
+        }
+
+        arr[j + 1] = temp; //Step 5
+    }
+}
