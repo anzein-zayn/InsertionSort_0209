4122291185a17dc935f5161c192b5078a64cec53
anzein-zayn <andy.zaky.ft24@mail.umy.ac.id>
2025-03-18 21:50:16 +0700

    Message For

--- ContohInsertionSort.cpp
+++ ContohInsertionSort.cpp
@@ -24,3 +24,10 @@ void input ()
     cout << "======================" << endl;
     cout << "Masukan Element Array" << endl;
     cout << "======================" << endl;
+    
+    for (int i = 0; i < n; i++) // Menggunakan perulangan for untuk menyimpan data pada array
+    {
+        cout << "Data ke-" << (i + 1) << ": "; // Memasukkan atau menginputkan nilai data n
+        cin >> arr [i]; //Menyimpan nilai data n kedalam array arr
+    }
+}
