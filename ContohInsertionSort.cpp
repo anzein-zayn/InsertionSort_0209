9a4947d2ec89b43ffb6642635426799a8535cb4b
anzein-zayn <andy.zaky.ft24@mail.umy.ac.id>
2025-03-18 22:09:45 +0700

    Message Looping For

--- ContohInsertionSort.cpp
+++ ContohInsertionSort.cpp
@@ -62,4 +62,10 @@ void display()
     cout << "=================================" << endl; //Output ke layar
     cout << "Element Array yang telah tersusun" << endl; //Output ke layar
     cout << "=================================" << endl; //Output ke layar
+
+    for (int j = 0; j < n; j++)
+    {//Looping j dimulai dari 0 hingga n-1
+        cout << arr[j] << endl;//Output ke layar
+    }
+    cout << endl;//Output baris kosong
 }
